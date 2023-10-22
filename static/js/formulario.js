8adee79081b27f105d09411ed3276e4109309a65
Nahu2412 <diegosoles99@gmail.com>
2023-10-21 22:15:05 -0300

    Cambio del formulario

--- static/js/formulario.js
+++ static/js/formulario.js
@@ -1,6 +1,6 @@
 const formulario = document.querySelector('#contact-register');
 
-const crear_advertencia=(divId,texto,input_id)=>{
+const crear_advertencia=(divId,texto,input_id,is_dual)=>{
     let p = document.createElement('p');
     let id_c = "#"+divId;
     let container = document.querySelector(id_c+"-container");
@@ -8,27 +8,38 @@ const crear_advertencia=(divId,texto,input_id)=>{
     p.innerHTML = texto;
     container.innerHTML="";
     container.insertAdjacentElement('beforeend',p);
-    input_id.className = "error";
+    if(is_dual){
+        input_id.className = "error-dual";
+    }else{
+        input_id.className = "error";
+    }
+    
 }
 
 const validacion = (event) =>{
     event.preventDefault();
     let nombre = document.querySelector("#nombre");
     let correo = document.querySelector("#correo");
-    let mensaje = document.querySelector("#mensaje");
+    let ciudad = document.querySelector("#Ciudad");
+    let provincia = document.querySelector("#Provincia");
+
     if(nombre.value==""){
-        crear_advertencia("name-form","Por favor ingresa un nombre",nombre);
+        crear_advertencia("name-form","Por favor ingresa un nombre",nombre,true);
         return false;
     }
     if(correo.value==""){
-        crear_advertencia("correo-form","Por favor ingresa un correo valido",correo);
+        crear_advertencia("correo-form","Por favor ingresa un correo valido",correo,true);
         return false;
     }
-    if(mensaje.value==""){
-        crear_advertencia("mensaje-form","No te olvides de dejar tu mensaje",mensaje)
+    if(ciudad.value==""){
+        crear_advertencia("Ciudad-form","Ingresa una ciudad",ciudad,true);
         return false;
     }
-    console.log(nombre.value);
+    if(provincia.value==""){
+        crear_advertencia("Provincia-form","Ingresa una provincia",provincia,true);
+        return false;
+    }
+    console.log("Formulario completo");
     
 }
 
