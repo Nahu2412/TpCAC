befab68cb995e21c3a648fd2d1acbb3a40ad676f
Nahu2412 <diegosoles99@gmail.com>
2026-06-30 11:26:43 -0300

    changes to product attributes and backend access paths

--- static/js/crud.js
+++ static/js/crud.js
@@ -1,52 +1,39 @@
 const{ createApp } = Vue;
+
 createApp({
   data() {
     return {
         productos : [],
-        api_server:"http://127.0.0.1:8000",
+        api_backend:"http://localhost:8080",
         id_producto:'',
-        nombre:'',
-        talle:'',
-        circunferencia:'',
-        banner:null,
-        material:'',
-        tamaniocopa:'',
-        tamanioala:'',
-        descripcion:'',
-        precio:''
+        name:'',
+        price:'',
+        description:'',
+        image:''
     };
   },
   methods: {
-    sendFormData(url, formData,method) {
+    sendFormData(url, formData,methodParam) {
         fetch(url, {
-          method: method,
-          body: formData,
+          method: methodParam,
+          headers: {
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify(formData),
         })
-        .then((response) => response.json())
+        .then((response) => response.text())
         .then((data) => {
-            console.log("datos recibidos:",data)
-            this.nombre='',
-            this.talle='',
-            this.circunferencia='',
-            this.banner=null,
-            this.material='',
-            this.tamaniocopa='',
-            this.tamanioala='',
-            this.descripcion='',
-            this.precio=''
-            if(method==='POST'){
-                this.getProductos(`${this.api_server}/api/productos`);
-            }
+            this.name='',
+            this.price='',
+            this.description='',
+            this.image=''
         })
         .catch((error) => {
             console.error("Error al enviar el formulario:", error);
         });
     },
-    onFileChange(event) {
-        this.banner = event.target.files[0];
-    },
     getProductos() {
-        fetch(`${this.api_server}/api/productos/`)
+        fetch(`${this.api_backend}/api/v1/product/all`)
             .then((response) => response.json())
             .then((data) => {
                 this.productos = data;
@@ -55,60 +42,44 @@ createApp({
                 console.error(err);
             });
     },
-     getProducto(id_producto) {
-         fetch(`${this.api_server}/api/producto/${id_producto}/`, {
+    getProducto(id_producto) {
+         fetch(`${this.api_backend}/api/v1/product/${id_producto}`, {
              method: 'GET',
          })
          .then((response) => response.json())
          .then((data) => {
              this.id_producto = data.id;
-             this.nombre = data.nombre;
-             this.talle = data.talle;
-             this.circunferencia = data.circunferencia;
-             this.material = data.material;
-             this.tamaniocopa = data.tamaniocopa;
-             this.tamanioala = data.tamanioala;
-             this.descripcion = data.descripcion;
-             this.precio = data.precio;
-             console.log(data);
+             this.name = data.name;
+             this.price = data.price;
+             this.description = data.description;
+             this.image = data.image;
          })
          .catch((error) => {
              console.error("Error al enviar el formulario:", error);
          });
      },
-   
     saveProducto() {
-        const formData = new FormData();
-        formData.append('nombre', this.nombre);
-        formData.append('talle', this.talle);
-        formData.append('circunferencia', this.circunferencia);
-        formData.append('material', this.material);
-        formData.append('tamaniocopa', this.tamaniocopa);
-        formData.append('tamanioala', this.tamanioala);
-        formData.append('descripcion',this.descripcion);
-        formData.append('precio',this.precio);
-        formData.append('banner', this.banner);
-
-        console.log(formData.values);
+        const productData = {
+            name: this.name,
+            price: parseInt(this.price),
+            description: this.description,
+            image: this.image
+        };
 
         if(this.id_producto){ 
-            //this.sendFormData(`${this.api_server}/api/actualizar_producto/${this.id_producto}/`, formData,'PUT');
-            this.sendFormData(`${this.api_server}/api/update_producto/${this.id_producto}/`, formData,'PUT');
-            alert("Producto actualizado");
-        }else{// Si no creo el registro en el backend
-            this.sendFormData(`${this.api_server}/api/crear_producto/`, formData,'POST');
-            alert("Producto Creado");
+            this.sendFormData(`${this.api_backend}/api/v1/product/edit/${this.id_producto}/`, productData,'PUT');
+        }else{
+            this.sendFormData(`${this.api_backend}/api/v1/product/create`, productData,'POST');
         }
     },
     deleteProducto(id_producto) {
-        fetch(`${this.api_server}/api/eliminar_producto/${id_producto}`, {
+        fetch(`${this.api_backend}/api/v1/product/delete/${id_producto}`, {
             method: 'DELETE',
         })
         .then((response) => response.json())
         .then((data) => {
             alert("Producto Eliminado");
-
-            this.getProductos(`${this.api_server}/api/productos/`);
+            this.getProductos(`${this.api_backend}/api/v1/product/all`);
         })
         .catch((error) => {
             console.error("Error al eliminar", error);
